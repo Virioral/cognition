dba89561bbeb2a5433c29e1a579fc247f81d7a05
Kevin <legraskevin@outlook.com>
2021-10-28 18:51:43 +0200

    :lipstick: create timer

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-let __data = {'time' : {'etape1' : '','etape2' : '','etape3' : '','etape4' : '','etape5' : '','etape6' : ''}};
-let timer = setInterval(function() { time++ }, 1000);
-let time = 0;
+let __data = {'time' : {'etape1' : '','etape2' : '','etape3' : '','etape4' : '','etape5' : '','etape6' : '','global' : ''}};
+let timer = null
+let seconde = 0;
+let minute = 0;
 let objectToFind = ['Poire','poire','poire','poire','poire','poire'];
+let etapes = ['etape1','etape2','etape3','etape4','etape5','etape6'];
 
 function submitForm(){
     $('form').on('submit', function(e) {
@@ -16,14 +18,17 @@ function submitForm(){
 function beforeStart(){
     $('.before_start').on('click', function() {
         nextStep('card_presentation','card_before_start');
+        displayObjectToFind();
         startGame();
     });
 }
 
 function startGame(){
     $('.start_game').on('click', function() {
-        nextStep('card_before_start','etape1');
-        time = 0;
+        console.log(etapes[0]);
+        nextStep('card_before_start',etapes[0]);
+        seconde = 0;
+        initTimer();
     })   
 }
 
@@ -33,24 +38,42 @@ function nextStep(oldstep,newstep){
 }
 
 function initTimer(){
-    timer = setInterval(function() {
-        time++;    
-    }, 1000);
+    timer = setInterval(function() { 
+        seconde++;
+        if(seconde == 60){
+            minute++;
+            seconde = 0;
+        }
+        $('.minute').text(minute + ' min');
+        $('.seconde').text(seconde + ' s');
+     }, 1000);
 }
 
 function saveTimer(name){
-    console.log("saveTimer = "+ timer + "secondes" );
-    __data['time'][name]=timer;
-    console.log(timer);
+    console.log("saveTimer = "+ seconde + "secondes" );
+    __data['time'][name]=time;
     clearInterval(timer);
-    time = 0;
+    seconde = 0;
+    console.log(__data);
 }
 
 function checkclick(elem){    
     if(objectToFind[0] == elem.text()){
         saveTimer(elem.closest("section").attr('class'));
         objectToFind.shift();
+        etapes.shift();
+        if($(objectToFind).empty()){
+            displayObjectToFind();
+            nextStep('etape1','card_before_start');
+        }
+        else{
+            displayEnd();
+        }
     }
+}
+
+function displayObjectToFind(){
+    $('.objectToFind').text(objectToFind[0])
 }
 
 
@@ -58,6 +81,5 @@ $('document').ready(function() {
     submitForm();
     $('section nav a').on('click', function(){
         checkclick($(this));
-
     });
 })
