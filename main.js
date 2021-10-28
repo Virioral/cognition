f36a255707987aa1990d88d86123e1f69907b28a
Kevin <legraskevin@outlook.com>
2021-10-28 17:57:15 +0200

    :construction: fonction timer

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-let __data = {};
+let __data = {'time' : {'etape1' : '','etape2' : '','etape3' : '','etape4' : '','etape5' : '','etape6' : ''}};
+let timer = setInterval(function() { time++ }, 1000);
+let time = 0;
+let objectToFind = ['Poire','poire','poire','poire','poire','poire'];
 
 function submitForm(){
     $('form').on('submit', function(e) {
@@ -13,14 +16,15 @@ function submitForm(){
 function beforeStart(){
     $('.before_start').on('click', function() {
         nextStep('card_presentation','card_before_start');
+        startGame();
     });
-    startGame();
 }
 
 function startGame(){
     $('.start_game').on('click', function() {
         nextStep('card_before_start','etape1');
-    })
+        time = 0;
+    })   
 }
 
 function nextStep(oldstep,newstep){
@@ -28,16 +32,32 @@ function nextStep(oldstep,newstep){
     $('.' + newstep).removeClass('d-none');
 }
 
-
 function initTimer(){
+    timer = setInterval(function() {
+        time++;    
+    }, 1000);
 }
 
-function saveTimer(){
+function saveTimer(name){
+    console.log("saveTimer = "+ timer + "secondes" );
+    __data['time'][name]=timer;
+    console.log(timer);
+    clearInterval(timer);
+    time = 0;
+}
+
+function checkclick(elem){    
+    if(objectToFind[0] == elem.text()){
+        saveTimer(elem.closest("section").attr('class'));
+        objectToFind.shift();
+    }
 }
 
 
 $('document').ready(function() {
     submitForm();
-    console.log(__data);
-      
+    $('section nav a').on('click', function(){
+        checkclick($(this));
+
+    });
 })
